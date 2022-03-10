007a6fdfdcd8752d865fde86b54ec721856e02f4
stewarteaton <stewarteaton05@gmail.com>
2022-03-10 07:42:30 -0500

    remove console.log

--- Server/app.js
+++ Server/app.js
@@ -5,27 +5,21 @@ const functions = require('./functions/functions')
 
 
 app.get('/txs', async (req, res) => {
-    let action = req.query.action
-    let date = req.query.date
-    let brandId = req.query.brandId
 
     // If no params passed - get credits for each account
     if (!req.query.action && !req.query.date && !req.query.brandId) {
-        console.log('Base Case')
         let accountCredits = await functions.getCredits()
         res.send(accountCredits)
     }
 
     // Query Specific Brand and Date
     else if (!req.query.action && req.query.date && req.query.brandId) {
-        console.log('Brand & Date');
         let accountCredits = await functions.getSpecificCredits(req.query.brandId, req.query.date)
         res.send(accountCredits)
     }
 
     // Query Specific Brand and Date and Action
     else if (req.query.action && req.query.date && req.query.brandId) {
-        console.log('Action & Brand & Date');
         let accountCredits = await functions.getActionSpecificCredits(req.query.action, req.query.brandId, req.query.date)
         res.send(accountCredits)
     }
